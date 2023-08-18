cc9775a6e42831e15cfc8fafbcdae39dc36c9f06
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-08-18 14:57:14 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'lists/index'
   root to: 'lists#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: %i[show create new] do
+  resources :lists, only: %i[index show create new destroy] do
     resources :bookmarks, only: %i[show create new]
   end
   resources :bookmarks, only: %i[destroy]
